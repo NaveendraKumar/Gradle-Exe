6998cfb896b066e50af7ae2eb72d606d718604e0
NaveendraKumar <naveendrakumar37@gmail.com>
2023-03-20 15:22:12 +0530

    New Modoification in the Gradle Project

---
+++ src/test/java/com/Navin/GradleProjectTest.java
@@ -0,0 +1,11 @@
+package com.Navin;
+
+import org.junit.Test;
+
+public class GradleProjectTest {
+
+@Test
+   public void verifyNoExceptionThrown()  {
+          GradleProject1.main(new String[]{ });
+  }
+}
